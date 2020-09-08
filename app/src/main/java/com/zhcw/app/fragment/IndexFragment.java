753e2a0d23877e7134169b9f9b032d08f795154f
yangchun <yangchun@zhcw.com>
2020-09-08 17:35:36 +0800

    c 2

---
+++ app/src/main/java/com/zhcw/app/fragment/IndexFragment.java
@@ -0,0 +1,15 @@
+package com.zhcw.app.fragment;
+
+import com.zhcw.lib.base.BaseFragment;
+
+public class IndexFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
